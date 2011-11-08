85edf54222b40fa2edecf4d78f118159202a83e3
igagis <igagis@75acf142-8502-5b27-bcb6-60bf44c96ba3>
2011-11-08 10:08:28 +0000

    minor changes

--- trunk/tests/playwav/main.cpp
+++ trunk/tests/playwav/main.cpp
@@ -3,13 +3,10 @@
 #include <ting/FSFile.hpp>
 
 
-
-int main(int argc, char *argv[]){
-	aumiks::Lib aumiksLibrary(100, aumiks::STEREO_16_44100);
-	
+void Play(const std::string& fileName){
 	ting::Ref<aumiks::WavSound> snd;
 	{
-		ting::FSFile fi("sample44100stereo16.wav");
+		ting::FSFile fi(fileName);
 		snd = aumiks::WavSound::LoadWAV(fi);
 	}
 
@@ -21,6 +18,14 @@ int main(int argc, char *argv[]){
 //		TRACE(<< "Loop" << std::endl)
 		ting::Thread::Sleep(50);
 	}
+}
 
+
+int main(int argc, char *argv[]){
+	aumiks::Lib aumiksLibrary(100, aumiks::STEREO_16_44100);
+	
+//	Play("sample44100mono16.wav");
+	Play("sample44100stereo16.wav");
+	
 	return 0;
 }
