361a3273163c3427378e4c98b5c5663db8e479fa
igagis <igagis@75acf142-8502-5b27-bcb6-60bf44c96ba3>
2011-11-12 15:33:51 +0000

    playing 6 different sample formats

--- trunk/tests/playwav/main.cpp
+++ trunk/tests/playwav/main.cpp
@@ -4,6 +4,7 @@
 
 
 void Play(const std::string& fileName){
+	TRACE_ALWAYS(<< "Playing " << fileName << std::endl)
 	ting::Ref<aumiks::WavSound> snd = aumiks::WavSound::LoadWAV(fileName);
 
 	ASSERT(snd)
@@ -20,6 +21,10 @@ void Play(const std::string& fileName){
 int main(int argc, char *argv[]){
 	aumiks::Lib aumiksLibrary(100, aumiks::STEREO_16_44100);
 	
+	Play("sample11025mono16.wav");
+	Play("sample11025stereo16.wav");
+	Play("sample22050mono16.wav");
+	Play("sample22050stereo16.wav");
 	Play("sample44100mono16.wav");
 	Play("sample44100stereo16.wav");
 	
