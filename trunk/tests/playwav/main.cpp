4933b19849b47ee4824ca5bf2fcc27709c178ec5
igagis <igagis@75acf142-8502-5b27-bcb6-60bf44c96ba3>
2011-11-18 20:53:33 +0000

    move samples to separate directory

--- trunk/tests/playwav/main.cpp
+++ trunk/tests/playwav/main.cpp
@@ -23,72 +23,72 @@ int main(int argc, char *argv[]){
 		TRACE_ALWAYS(<< "Opening audio playback device: Mono 11025" << std::endl)
 		aumiks::Lib aumiksLibrary(100, aumiks::MONO_16_11025);
 
-		Play("sample11025mono16.wav");
-		Play("sample11025stereo16.wav");
-		Play("sample22050mono16.wav");
-		Play("sample22050stereo16.wav");
-		Play("sample44100mono16.wav");
-		Play("sample44100stereo16.wav");
+		Play("../samples/sample11025mono16.wav");
+		Play("../samples/sample11025stereo16.wav");
+		Play("../samples/sample22050mono16.wav");
+		Play("../samples/sample22050stereo16.wav");
+		Play("../samples/sample44100mono16.wav");
+		Play("../samples/sample44100stereo16.wav");
 	}
 	
 	{
 		TRACE_ALWAYS(<< "Opening audio playback device: Stereo 11025" << std::endl)
 		aumiks::Lib aumiksLibrary(100, aumiks::STEREO_16_11025);
 
-		Play("sample11025mono16.wav");
-		Play("sample11025stereo16.wav");
-		Play("sample22050mono16.wav");
-		Play("sample22050stereo16.wav");
-		Play("sample44100mono16.wav");
-		Play("sample44100stereo16.wav");
+		Play("../samples/sample11025mono16.wav");
+		Play("../samples/sample11025stereo16.wav");
+		Play("../samples/sample22050mono16.wav");
+		Play("../samples/sample22050stereo16.wav");
+		Play("../samples/sample44100mono16.wav");
+		Play("../samples/sample44100stereo16.wav");
 	}
 	
 	{
 		TRACE_ALWAYS(<< "Opening audio playback device: Mono 22050" << std::endl)
 		aumiks::Lib aumiksLibrary(100, aumiks::MONO_16_22050);
 
-		Play("sample11025mono16.wav");
-		Play("sample11025stereo16.wav");
-		Play("sample22050mono16.wav");
-		Play("sample22050stereo16.wav");
-		Play("sample44100mono16.wav");
-		Play("sample44100stereo16.wav");
+		Play("../samples/sample11025mono16.wav");
+		Play("../samples/sample11025stereo16.wav");
+		Play("../samples/sample22050mono16.wav");
+		Play("../samples/sample22050stereo16.wav");
+		Play("../samples/sample44100mono16.wav");
+		Play("../samples/sample44100stereo16.wav");
 	}
 	
 	{
 		TRACE_ALWAYS(<< "Opening audio playback device: Stereo 22050" << std::endl)
 		aumiks::Lib aumiksLibrary(100, aumiks::STEREO_16_22050);
 
-		Play("sample11025mono16.wav");
-		Play("sample11025stereo16.wav");
-		Play("sample22050mono16.wav");
-		Play("sample22050stereo16.wav");
-		Play("sample44100mono16.wav");
-		Play("sample44100stereo16.wav");
+		Play("../samples/sample11025mono16.wav");
+		Play("../samples/sample11025stereo16.wav");
+		Play("../samples/sample22050mono16.wav");
+		Play("../samples/sample22050stereo16.wav");
+		Play("../samples/sample44100mono16.wav");
+		Play("../samples/sample44100stereo16.wav");
 	}
 	
 	{
 		TRACE_ALWAYS(<< "Opening audio playback device: Mono 44100" << std::endl)
 		aumiks::Lib aumiksLibrary(100, aumiks::MONO_16_44100);
 
-		Play("sample11025mono16.wav");
-		Play("sample11025stereo16.wav");
-		Play("sample22050mono16.wav");
-		Play("sample22050stereo16.wav");
-		Play("sample44100mono16.wav");
-		Play("sample44100stereo16.wav");
+		Play("../samples/sample11025mono16.wav");
+		Play("../samples/sample11025stereo16.wav");
+		Play("../samples/sample22050mono16.wav");
+		Play("../samples/sample22050stereo16.wav");
+		Play("../samples/sample44100mono16.wav");
+		Play("../samples/sample44100stereo16.wav");
 	}
 	
 	{
 		TRACE_ALWAYS(<< "Opening audio playback device: Stereo 44100" << std::endl)
 		aumiks::Lib aumiksLibrary(100, aumiks::STEREO_16_44100);
 
-		Play("sample11025mono16.wav");
-		Play("sample11025stereo16.wav");
-		Play("sample22050mono16.wav");
-		Play("sample22050stereo16.wav");
-		Play("sample44100mono16.wav");
-		Play("sample44100stereo16.wav");
+		Play("../samples/sample11025mono16.wav");
+		Play("../samples/sample11025stereo16.wav");
+		Play("../samples/sample22050mono16.wav");
+		Play("../samples/sample22050stereo16.wav");
+		Play("../samples/sample44100mono16.wav");
+		Play("../samples/sample44100stereo16.wav");
 	}
 	
 	return 0;
