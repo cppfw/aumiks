f2135a67d6238409db235f3cb69a9d29a0589b3f
igagis <igagis@75acf142-8502-5b27-bcb6-60bf44c96ba3>
2011-12-11 18:09:34 +0000

    XAudio2 backend stub added

--- src/aumiks/aumiks.cpp
+++ src/aumiks/aumiks.cpp
@@ -34,9 +34,18 @@ THE SOFTWARE. */
 
 #include "aumiks.hpp"
 
+#ifdef WIN32
+#include "../backend/XAudio2Backend.hpp"
+
+#elif defined(__linux__)
 #include "../backend/PulseAudioBackend.hpp"
 //#include "backend/ALSABackend.hpp"
 //#include "backend/ESoundBackend.hpp"
+
+#else
+#error "Unknown OS"
+#endif
+
 
 using namespace aumiks;
 
@@ -88,7 +97,13 @@ unsigned BufferSizeInSamples(unsigned bufferSizeMillis, E_Format format){
 
 Lib::Lib(ting::u16 bufferSizeMillis, aumiks::E_Format format) :
 		mixerBuffer(Lib::CreateMixerBuffer(BufferSizeInSamples(bufferSizeMillis, format), format)),
+#ifdef WIN32
+		audioBackend(XAudio2Backend::New(BufferSizeInFrames(bufferSizeMillis, format), format))
+#elif defined(__linux__)
 		audioBackend(PulseAudioBackend::New(BufferSizeInFrames(bufferSizeMillis, format), format))
+#else
+#error "undefined OS"
+#endif
 {}
 
 
