4c2806e5b6559e685afe86c1efbe571764c09021
igagis <igagis@75acf142-8502-5b27-bcb6-60bf44c96ba3>
2011-12-12 20:13:49 +0000

    clear the initial buffer contents in DirectSound

--- trunk/src/aumiks/aumiks.hpp
+++ trunk/src/aumiks/aumiks.hpp
@@ -76,19 +76,19 @@ class AudioBackend;
 class Effect : public ting::RefCounted{
 	friend class aumiks::Channel;
 	friend class aumiks::Lib;
-	
+
 	typedef std::list<ting::Ref<aumiks::Effect> > T_EffectsList;
 	typedef T_EffectsList::iterator T_EffectsIter;
-	
+
 public:
 
 	/**
 	 * @brief Called every time when the Channel is about to start playing.
 	 * Called from separate thread.
 	 * Default implementation does nothing. Override this method if needed.
-     */
+	 */
 	virtual void Init_ts(){}
-	
+
 	/**
 	 * @brief A result which should be returned by effect application method.
 	 */
@@ -102,7 +102,7 @@ public:
 		 * playing life time in any way.
 		 */
 		NORMAL,
-		
+
 		/**
 		 * @brief Effect is still producing output.
 		 * Return this if effect is producing output. If some of the effects added to the channel returns this value
@@ -113,7 +113,7 @@ public:
 		 * has returned STOP_SOUND then the channel playing will be stopped, no matter if this effect returns CONTINUE.
 		 */
 		CONTINUE,
-		
+
 		/**
 		 * @brief Effect has request to stop the sound.
 		 * Return this value if effect has fully suppressed the sound and there is no reason to
@@ -123,48 +123,48 @@ public:
 		 */
 		STOP_SOUND
 	};
-	
+
 	/**
 	 * @brief Called when effect is to be applied to a portion of a playing sound.
 	 * Depending on the output sound format the corresponding method is called.
 	 * Note, that when effect is used as a global effect, then the return value from
 	 * this method is ignored. It only matters when effect is added to channel.
-     * @param buf - buffer containing portion of sound data.
+	 * @param buf - buffer containing portion of sound data.
 	 * @param soundStopped - true if sound has finished playing, false otherwise.
-     * @return One of the E_Result values. See E_Result description for more info.
-     */
+	 * @return One of the E_Result values. See E_Result description for more info.
+	 */
 	virtual E_Result ApplyToBuf11025Mono16(ting::Buffer<ting::s32>& buf, bool soundStopped){return NORMAL;}
-	
+
 	/**
 	 * @brief Called when effect is to be applied to a portion of a playing sound.
 	 * See description of Effect::ApplyToSmpBuf11025Mono16() method.
-     */
+	 */
 	virtual E_Result ApplyToBuf11025Stereo16(ting::Buffer<ting::s32>& buf, bool soundStopped){return NORMAL;}
-	
+
 	/**
 	 * @brief Called when effect is to be applied to a portion of a playing sound.
 	 * See description of Effect::ApplyToSmpBuf11025Mono16() method.
-     */
+	 */
 	virtual E_Result ApplyToBuf22050Mono16(ting::Buffer<ting::s32>& buf, bool soundStopped){return NORMAL;}
-	
+
 	/**
 	 * @brief Called when effect is to be applied to a portion of a playing sound.
 	 * See description of Effect::ApplyToSmpBuf11025Mono16() method.
-     */
+	 */
 	virtual E_Result ApplyToBuf22050Stereo16(ting::Buffer<ting::s32>& buf, bool soundStopped){return NORMAL;}
-	
+
 	/**
 	 * @brief Called when effect is to be applied to a portion of a playing sound.
 	 * See description of Effect::ApplyToSmpBuf11025Mono16() method.
-     */
+	 */
 	virtual E_Result ApplyToBuf44100Mono16(ting::Buffer<ting::s32>& buf, bool soundStopped){return NORMAL;}
-	
+
 	/**
 	 * @brief Called when effect is to be applied to a portion of a playing sound.
 	 * See description of Effect::ApplyToSmpBuf11025Mono16() method.
-     */
+	 */
 	virtual E_Result ApplyToBuf44100Stereo16(ting::Buffer<ting::s32>& buf, bool soundStopped){return NORMAL;}
-	
+
 private:
 	template <unsigned freq, unsigned chans> inline E_Result ApplyToBufImpl(ting::Buffer<ting::s32>& buf, bool soundStopped);
 };
@@ -179,20 +179,20 @@ class Channel : public ting::RefCounted{
 	friend class aumiks::Lib;
 
 	ting::Inited<volatile bool, false> isPlaying;
-	
+
 	bool soundStopped;//used to indicate that sound has finished playing, but effects are still playing.
 
 	ting::Inited<volatile bool, false> stopFlag;//indicates that playing should stop immediately
-	
+
 private:
 	Effect::T_EffectsList effects;
-	
+
 	inline void InitEffects(){
 		for(Effect::T_EffectsIter i = this->effects.begin(); i != this->effects.end(); ++i){
 			(*i)->Init_ts();
 		}
 	}
-	
+
 	inline void RemoveEffect(const ting::Ref<Effect>& e){
 		for(Effect::T_EffectsIter i = this->effects.begin(); i != this->effects.end(); ++i){
 			if((*i) == e){
@@ -201,7 +201,7 @@ private:
 			}
 		}
 	}
-	
+
 	template <unsigned freq, unsigned chans> inline bool ApplyEffectsToSmpBuf(ting::Buffer<ting::s32>& buf){
 		bool ret = this->soundStopped;
 		bool stopRequested = false;
@@ -229,59 +229,59 @@ private:
 		return ret || stopRequested;
 	}
 
-	
+
 protected:
 	Channel(){}
-	
+
 public:
 
 	/**
 	 * @brief Check if sound is currently playing.
-     * @return true if channel is playing.
+	 * @return true if channel is playing.
 	 * @return false otherwise.
-     */
+	 */
 	inline bool IsPlaying()const{
 		return this->isPlaying;
 	}
 
 	/**
 	 * @brief Start playing of this channel.
-     */
+	 */
 	inline void Play();
 
 	/**
 	 * @brief Stop playing of this channel.
-     */
+	 */
 	inline void Stop(){
 		this->stopFlag = true;
 	}
-	
+
 	/**
 	 * @brief Add effect to the channel.
 	 * It is allowed to add effects during channel playing.
 	 * The single effect instance can only be added to one channel. Adding single
 	 * Effect instance to more than one channel will result in undefined behavior.
-     * @param effect - the effect to add.
-     */
+	 * @param effect - the effect to add.
+	 */
 	inline void AddEffect_ts(const ting::Ref<aumiks::Effect>& effect);
-	
+
 	/**
 	 * @brief Remove effect from the channel.
 	 * It is allowed to remove effects during channel playing.
-     * @param effect - effect to remove.
-     */
+	 * @param effect - effect to remove.
+	 */
 	inline void RemoveEffect_ts(const ting::Ref<aumiks::Effect>& effect);
-	
+
 	/**
 	 * @brief Remove all effects from channel.
-     */
+	 */
 	inline void RemoveAllEffects_ts();
 protected:
 	/**
 	 * @brief Called when channel has been added to pool of playing channels.
 	 */
 	virtual void OnStart(){}
-	
+
 	/**
 	 * @brief Called when channel has been removed from pool of playing channels.
 	 */
@@ -295,40 +295,40 @@ protected:
 	 * Note, that channel playing may continue even if sound has stopped playing, this is
 	 * possible if there are any effects added to this channel which keeps playing, for example
 	 * an echo effect.
-     * @param buf - the sample buffer to fill with the data to play.
-     * @return true if sound playing has finished.
+	 * @param buf - the sample buffer to fill with the data to play.
+	 * @return true if sound playing has finished.
 	 * @return false otherwise.
-     */
+	 */
 	virtual bool FillSmpBuf11025Mono16(ting::Buffer<ting::s32>& buf){return true;}
-	
+
 	/**
 	 * @brief This function is called when more data to play is needed.
 	 * See description of Channel::FillSmpBuf11025Mono16() method.
-     */
+	 */
 	virtual bool FillSmpBuf11025Stereo16(ting::Buffer<ting::s32>& buf){return true;}
-	
+
 	/**
 	 * @brief This function is called when more data to play is needed.
 	 * See description of Channel::FillSmpBuf11025Mono16() method.
-     */
+	 */
 	virtual bool FillSmpBuf22050Mono16(ting::Buffer<ting::s32>& buf){return true;}
-	
+
 	/**
 	 * @brief This function is called when more data to play is needed.
 	 * See description of Channel::FillSmpBuf11025Mono16() method.
-     */
+	 */
 	virtual bool FillSmpBuf22050Stereo16(ting::Buffer<ting::s32>& buf){return true;}
-	
+
 	/**
 	 * @brief This function is called when more data to play is needed.
 	 * See description of Channel::FillSmpBuf11025Mono16() method.
-     */
+	 */
 	virtual bool FillSmpBuf44100Mono16(ting::Buffer<ting::s32>& buf){return true;}
-	
+
 	/**
 	 * @brief This function is called when more data to play is needed.
 	 * See description of Channel::FillSmpBuf11025Mono16() method.
-     */
+	 */
 	virtual bool FillSmpBuf44100Stereo16(ting::Buffer<ting::s32>& buf){return true;}
 };
 
@@ -344,27 +344,27 @@ enum E_Format{
 	 * @brief Mono 11025 Hz.
 	 */
 	MONO_16_11025,
-	
+
 	/**
 	 * @brief Mono 22050 Hz.
 	 */
 	MONO_16_22050,
-	
+
 	/**
 	 * @brief Mono 44100 Hz.
 	 */
 	MONO_16_44100,
-	
+
 	/**
 	 * @brief Stereo 11025 Hz.
 	 */
 	STEREO_16_11025,
-	
+
 	/**
 	 * @brief Stereo 22050 Hz.
 	 */
 	STEREO_16_22050,
-	
+
 	/**
 	 * @brief Stereo 44100 Hz.
 	 */
@@ -413,19 +413,19 @@ unsigned SamplingRate(E_Format format);
 class Lib : public ting::Singleton<Lib>{
 	friend class aumiks::Channel;
 	friend class aumiks::AudioBackend;
-	
+
 	void AddEffectToChannel_ts(const ting::Ref<Channel>& ch, const ting::Ref<aumiks::Effect>& eff);
-	
+
 	void RemoveEffectFromChannel_ts(const ting::Ref<Channel>& ch, const ting::Ref<aumiks::Effect>& eff);
-	
+
 	void RemoveAllEffectsFromChannel_ts(const ting::Ref<Channel>& ch);
-	
+
 	void PlayChannel_ts(const ting::Ref<Channel>& ch);
-	
+
 	static inline void RemoveEffectFromChannelSync(const ting::Ref<Channel>& ch, const ting::Ref<aumiks::Effect>& eff){
 		ch->RemoveEffect(eff);
 	}
-	
+
 	static inline void AddEffectToChannelSync(const ting::Ref<Channel>& ch, const ting::Ref<aumiks::Effect>& eff){
 		ch->effects.push_back(eff);
 	}
@@ -439,13 +439,13 @@ public:
 	 * @param format - format of sound output.
 	 */
 	Lib(ting::u16 bufferSizeMillis = 100, aumiks::E_Format format = STEREO_16_22050);
-	
+
 	/**
 	 * @brief Mute sound output.
 	 * Mute the sound output. This is the same as the resulting sound volume would be set to zero.
 	 * I.e. all the computational resources for sound mixing etc. are still being consumed when sound is muted.
-     * @param muted - pass true to mute the sound, false to un-mute.
-     */
+	 * @param muted - pass true to mute the sound, false to un-mute.
+	 */
 	inline void SetMuted(bool muted){
 		this->mixerBuffer->isMuted = muted;
 	}
@@ -454,8 +454,8 @@ public:
 	 * @brief Unmute the sound.
 	 * Inversion of Lib::SetMuted() method.
 	 * See description of Lib::SetMuted() method for more info.
-     * @param unmuted - true to un-mute the sound, false to mute.
-     */
+	 * @param unmuted - true to un-mute the sound, false to mute.
+	 */
 	inline void SetUnmuted(bool unmuted){
 		this->SetMuted(!unmuted);
 	}
@@ -478,9 +478,9 @@ public:
 
 	/**
 	 * @brief Check if sound output is muted or not.
-     * @return true if sound output is muted.
+	 * @return true if sound output is muted.
 	 * @return false otherwise.
-     */
+	 */
 	inline bool IsMuted()const{
 		return this->mixerBuffer->isMuted;
 	}
@@ -496,32 +496,32 @@ private:
 		{}
 
 		template <unsigned freq, unsigned chans> inline bool FillSmpBufImpl(const ting::Ref<aumiks::Channel>& ch);
-		
+
 		ting::Array<ting::s32> mixBuf;
 		ting::Array<ting::s32> smpBuf;
 
 		ting::Inited<volatile bool, false> isMuted;
 	public:
 		virtual ~MixerBuffer(){}
-		
+
 	private:
 		inline void CleanMixBuf(){
 			memset(this->mixBuf.Begin(), 0, this->mixBuf.SizeInBytes());
 		}
-		
+
 		//return true if channel has finished playing and should be removed from playing channels pool
 		bool MixToMixBuf(const ting::Ref<aumiks::Channel>& ch);
 
 		void CopyToPlayBuf(ting::Buffer<ting::u8>& playBuf);
-		
+
 		virtual bool FillSmpBuf(const ting::Ref<aumiks::Channel>& ch) = 0;
-		
+
 		virtual bool ApplyEffectsToSmpBuf(const ting::Ref<aumiks::Channel>& ch) = 0;
-		
+
 		virtual void ApplyEffectsToMixBuf() = 0;
-		
+
 		void MixSmpBufToMixBuf();
-		
+
 		template <unsigned freq, unsigned chans> inline bool ApplyEffectsToSmpBufImpl(const ting::Ref<aumiks::Channel>& ch){
 			return ch->ApplyEffectsToSmpBuf<freq, chans>(this->smpBuf);
 		}
@@ -556,7 +556,7 @@ private:
 
 private:
 	void FillPlayBuf_ts(ting::Buffer<ting::u8>& playBuf);
-	
+
 	ting::Mutex mutex;
 
 	typedef std::list<ting::Ref<aumiks::Channel> > T_ChannelList;
@@ -566,54 +566,54 @@ private:
 	T_ChannelList channelsToAdd;
 
 	Effect::T_EffectsList effects;//list of effects applied to final mixing buffer
-	
+
 	template <unsigned freq, unsigned chans> inline void ApplyEffectsToMixBuf(ting::Buffer<ting::s32>& mixBuf){
 		for(Effect::T_EffectsIter i = this->effects.begin(); i != this->effects.end(); ++i){
 			(*i)->ApplyToBufImpl<freq, chans>(mixBuf, false);
 		}
 	}
-	
+
 	Effect::T_EffectsList effectsToAdd;
 	Effect::T_EffectsList effectsToRemove;
-	
+
 	ting::Inited<bool, false> clearEffects;//flag indicating that removing of all effects requested
-	
+
 public:
 	/**
 	 * @brief Add global effect.
 	 * Adds the effect to the list of global effects which are applied to the
 	 * final mixing buffer after all the playing channels are mixed.
-     * @param effect - effect to add.
-     */
+	 * @param effect - effect to add.
+	 */
 	inline void AddEffect_ts(const ting::Ref<Effect>& effect){
 		ASSERT(effect.IsValid())
-		
+
 		{
 			ting::Mutex::Guard mutexGuard(this->mutex);
-			
+
 			this->effectsToAdd.push_back(effect);
 		}
 	}
-	
+
 	/**
 	 * @brief Remove global effect.
 	 * Removes the effect from the list of global effects which are applied to the
 	 * final mixing buffer after all the playing channels are mixed.
-     * @param effect - effect to remove.
-     */
+	 * @param effect - effect to remove.
+	 */
 	inline void RemoveEffect_ts(const ting::Ref<Effect>& effect){
 		ASSERT(effect.IsValid())
-		
+
 		{
 			ting::Mutex::Guard mutexGuard(this->mutex);
-			
+
 			this->effectsToRemove.push_back(effect);
 		}
 	}
-	
+
 	inline void RemoveAllEffects_ts(){
 		ting::Mutex::Guard mutexGuard(this->mutex);
-		
+
 		this->clearEffects = true;
 	}
 private:
@@ -622,12 +622,12 @@ private:
 	typedef T_ChannelEffectPairsList::iterator T_ChannelEffectPairsIter;
 	T_ChannelEffectPairsList effectsToAddToChan;
 	T_ChannelEffectPairsList effectsToRemoveFromChan;
-	
+
 	//list of channels from which it is requested to remove all effects
 	T_ChannelList effectsToClear;
-	
+
 	const ting::Ptr<MixerBuffer> mixerBuffer;
-	
+
 	static ting::Ptr<MixerBuffer> CreateMixerBuffer(unsigned bufferSizeMillis, E_Format format);
 
 	//backend must be initialized after all the essential parts of aumiks are initialized,
@@ -693,7 +693,7 @@ template <> inline Effect::E_Result Effect::ApplyToBufImpl<44100, 1>(ting::Buffe
 template <> inline Effect::E_Result Effect::ApplyToBufImpl<44100, 2>(ting::Buffer<ting::s32>& buf, bool soundStopped){
 	return this->ApplyToBuf44100Stereo16(buf, soundStopped);
 }
-		
+
 
 
 /**
@@ -705,12 +705,12 @@ class Sound : public ting::RefCounted{
 protected:
 	Sound(){}
 public:
-	
+
 	/**
 	 * @brief Channel factory method.
 	 * Creates an instance of the channel which can later be used to play that sound.
-     * @return a newly created instance of the channel for this sound.
-     */
+	 * @return a newly created instance of the channel for this sound.
+	 */
 	virtual ting::Ref<aumiks::Channel> CreateChannel()const = 0;
 };
 
