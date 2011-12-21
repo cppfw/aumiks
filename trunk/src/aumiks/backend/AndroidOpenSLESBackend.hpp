9877f679d69985489afc86ff14407802ec88d2d8
igagis <igagis@75acf142-8502-5b27-bcb6-60bf44c96ba3>
2011-12-21 00:04:48 +0000

    OpenSL backend

--- trunk/src/aumiks/backend/AndroidOpenSLESBackend.hpp
+++ trunk/src/aumiks/backend/AndroidOpenSLESBackend.hpp
@@ -30,89 +30,246 @@ THE SOFTWARE. */
 
 
 
-#include <pulse/simple.h>
-#include <pulse/error.h>
+#include <SLES/OpenSLES.h>
+//#include <SLES/OpenSLES_Android.h> //TODO: remove this
 
 #include "WriteBasedBackend.hpp"
 
 
 namespace{
 
-class PulseAudioBackend : public WriteBasedBackend{
-	pa_simple *handle;
-
-	PulseAudioBackend(unsigned bufferSizeFrames, aumiks::E_Format format) :
-			WriteBasedBackend(bufferSizeFrames * aumiks::BytesPerFrame(format))
-	{
-		TRACE(<< "opening device" << std::endl)
-
-		pa_sample_spec ss;
-		ss.format = PA_SAMPLE_S16NE;//Native endian
-		ss.channels = aumiks::SamplesPerFrame(format);
-		ss.rate = aumiks::SamplingRate(format);
-
-		unsigned bufferSizeInBytes = bufferSizeFrames * aumiks::BytesPerFrame(format);
-		pa_buffer_attr ba;
-		ba.fragsize = bufferSizeInBytes;
-		ba.tlength = bufferSizeInBytes;
-		ba.minreq = bufferSizeInBytes / 2;
-		ba.maxlength = ba.tlength;
-		ba.prebuf = ba.tlength;
+class AndroidOpenSLESBackend{
+	
+	struct Engine{
+		SLObjectItf object; //object
+		SLEngineItf engine; //Engine interface
 		
-		pa_channel_map cm;
-		pa_channel_map_init_auto(&cm, ss.channels, PA_CHANNEL_MAP_WAVEEX);
-
-		int error;
-
-		this->handle = pa_simple_new(
-				0, // Use the default server.
-				"aumiks", // Our application's name.
-				PA_STREAM_PLAYBACK,
-				0, // Use the default device.
-				"Music", // Description of our stream.
-				&ss, // our sample format.
-				&cm, // channel map
-				&ba, // buffering attributes.
-				&error
-			);
-
-		if(!this->handle){
-			TRACE(<< "error opening PulseAudio connection (" << pa_strerror(error) << ")" << std::endl)
-			throw aumiks::Exc("error opening PulseAudio connection");
+		Engine(){
+			//create engine object
+			if(slCreateEngine(&this->object, 0, NULL, 0, NULL, NULL) != SL_RESULT_SUCCESS){
+				throw aumiks::Exc("OpenSLES: Creating engine object failed");
+			}
+			
+			//realize the engine
+			if((*this->object)->Realize(this->object, SL_BOOLEAN_FALSE) != SL_RESULT_SUCCESS){
+				this->Destroy();
+				throw aumiks::Exc("OpenSLES: Realizing engine object failed");
+			}
+			
+			//get the engine interface, which is needed in order to create other objects
+			if((*this->object)->GetInterface(this->object, SL_IID_ENGINE, &this->engine) != SL_RESULT_SUCCESS){
+				this->Destroy();
+				throw aumiks::Exc("OpenSLES: Obtaining Engine interface failed");
+			}
 		}
 		
-		this->Start();//start thread
+		~Engine(){
+			this->Destroy();
+		}
+		
+		void Destroy(){
+			(*this->object)->Destroy(this->object);
+		}
+		
+	private:
+		Engine(const Engine&);
+		Engine& operator=(const Engine&);
+	} engine;
+	
+	
+	
+	struct OutputMix{
+		SLObjectItf object;
+		
+		OutputMix(Engine& engine){
+			if((*engine.engine)->CreateOutputMix(engine.engine, &this->object, 0, NULL, NULL) != SL_RESULT_SUCCESS){
+				throw aumiks::Exc("OpenSLES: Creating output mix object failed");
+			}
+			
+			// realize the output mix
+			if((*this->object)->Realize(this->object, SL_BOOLEAN_FALSE) != SL_RESULT_SUCCESS){
+				this->Destroy();
+				throw aumiks::Exc("OpenSLES: Realizing output mix object failed");
+			}
+		}
+		
+		~OutputMix(){
+			this->Destroy();
+		}
+		
+		void Destroy(){
+			(*this->object)->Destroy(this->object);
+		}
+		
+	private:
+		OutputMix(const OutputMix&);
+		OutputMix& operator=(const OutputMix&);
+	} outputMix;
+	
+	
+	
+	struct Player{
+		SLObjectItf object;
+		SLPlayItf play;
+		SLAndroidSimpleBufferQueueItf bufferQueue;
+		
+		Player(Engine& engine, OutputMix& outputMix, unsigned bufferSizeFrames, aumiks::E_Format format){
+			//========================
+			// configure audio source
+			//TODO: remove this
+			//SLDataLocator_AndroidSimpleBufferQueue bufferQueue = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, 2}; //2 buffers in queue
+			SLDataLocator_BufferQueue bufferQueue = {SL_DATALOCATOR_BUFFERQUEUE, 2}; //2 buffers in queue
+			
+			unsigned numChannels = aumiks::SamplesPerFrame(format);
+			SLuint32 channelMask;
+			switch(numChannels){
+				case 1:
+					channelMask = SL_SPEAKER_FRONT_CENTER;
+					break;
+				case 2:
+					channelMask = SL_SPEAKER_FRONT_LEFT | SL_SPEAKER_FRONT_RIGHT;
+					break;
+				default:
+					ASSERT(false)
+					break;
+			}
+			SLDataFormat_PCM audioFormat = {
+				SL_DATAFORMAT_PCM,
+				numChannels, //number of channels
+				aumiks::SamplingRate(format) * 1000, //milliHertz
+				16, //bits per sample, if 16bits then sample is signed 16bit integer
+				16, //container size for sample, it can be bigger than sample itself. E.g. 32bit container for 16bits sample
+				channelMask, //which channels map to which speakers
+				SL_BYTEORDER_LITTLEENDIAN //we want little endian byte order
+			};
+			
+			SLDataSource audioSourceStruct = {&bufferQueue, &audioFormat};
+
+			//======================
+			// configure audio sink
+			SLDataLocator_OutputMix outputMixStruct = {SL_DATALOCATOR_OUTPUTMIX, outputMix.object};
+			SLDataSink audioSinkStruct = {&outputMixStruct, NULL};
+
+			//=====================
+			//=====================
+			// create audio player
+			const SLInterfaceID ids[1] = {SL_IID_BUFFERQUEUE};
+			const SLboolean req[1] = {SL_BOOLEAN_TRUE};
+			if((*engine.engine)->CreateAudioPlayer(
+					engine.engine,
+					&this->object,
+					&audioSourceStruct,
+					&audioSinkStruct,
+					1,
+					ids,
+					req
+				) != SL_RESULT_SUCCESS)
+			{
+				throw aumiks::Exc("OpenSLES: Creating player object failed");
+			}
+			
+			//realize the player
+			if((*this->object)->Realize(this->object, SL_BOOLEAN_FALSE) != SL_RESULT_SUCCESS){
+				this->Destroy();
+				throw aumiks::Exc("OpenSLES: Realizing player object failed");
+			}
+
+			//get the play interface
+			if((*this->object)->GetInterface(this->object, SL_IID_PLAY, &this->play) != SL_RESULT_SUCCESS){
+				this->Destroy();
+				throw aumiks::Exc("OpenSLES: Obtaining Play interface failed");
+			}
+
+			//get the buffer queue interface
+			if((*this->object)->GetInterface(this->object, SL_IID_BUFFERQUEUE, &this->bufferQueue) != SL_RESULT_SUCCESS){
+				this->Destroy();
+				throw aumiks::Exc("OpenSLES: Obtaining Play interface failed");
+			}
+
+			//register callback on the buffer queue
+			if((*this->bufferQueue)->RegisterCallback(
+					this->bufferQueue,
+					&AndroidOpenSLESBackend::Callback,
+					this //context to be passed to the callback
+				) != SL_RESULT_SUCCESS)
+			{
+				this->Destroy();
+				throw aumiks::Exc("OpenSLES: Registering callback on the buffer queue failed");
+			}
+		}
+		
+		~Player(){
+			this->Destroy();
+		}
+		
+		void Destroy(){
+			(*this->object)->Destroy(this->object);
+		}
+		
+	private:
+		Player(const Player&);
+		Player& operator=(const Player&);
+	} player;
+	
+	
+	
+	//this callback handler is called every time a buffer finishes playing
+	static void Callback(
+			SLBufferQueueItf queueItf,
+			SLuint32 eventFlags,
+			const void *pBuffer,
+			SLuint32 bufferSize,
+			SLuint32 dataUsed,
+			void *pContext
+		)
+	{
+		//TODO:
 	}
 	
-	//override
-	void Write(const ting::Buffer<ting::u8>& buf){
-//		ASSERT(buf.Size() == this->BufferSizeInBytes())
-
-		if(pa_simple_write(
-				this->handle,
-				buf.Begin(),
-				size_t(buf.SizeInBytes()),
-				0 // no error return
-			) < 0)
-		{
-			//TODO: handle error somehow, throw exception
-			//ignore error
-			TRACE(<< "pa_simple_write(): error" << std::endl)
+	
+	
+	/*
+	//this callback handler is called every time a buffer finishes playing
+	static void Callback(SLAndroidSimpleBufferQueueItf bq, void *context){
+		TODO:
+		assert(bq == bqPlayerBufferQueue);
+		assert(NULL == context);
+		// for streaming playback, replace this test by logic to find and fill the next buffer
+		if (--nextCount > 0 && NULL != nextBuffer && 0 != nextSize) {
+			SLresult result;
+			// enqueue another buffer
+			result = (*bqPlayerBufferQueue)->Enqueue(bqPlayerBufferQueue, nextBuffer, nextSize);
+			// the most likely other result is SL_RESULT_BUFFER_INSUFFICIENT,
+			// which for this code example would indicate a programming error
+			assert(SL_RESULT_SUCCESS == result);
+		}
+	}
+	*/
+	
+	
+	
+	//create buffered queue player
+	AndroidOpenSLESBackend(unsigned bufferSizeFrames, aumiks::E_Format format) :
+			outputMix(this->engine),
+			player(this->engine, this->outputMix, bufferSizeFrames, format)
+	{
+		// Set player to playing state
+		if((*player.play)->SetPlayState(player.play, SL_PLAYSTATE_PLAYING) != SL_RESULT_SUCCESS){
+			throw aumiks::Exc("OpenSLES: Setting player state to PLAYING failed");
 		}
 	}
 
 public:
 
-	~PulseAudioBackend(){
-		this->StopThread();
-		
-		ASSERT(this->handle)
-		pa_simple_free(this->handle);
+	~AndroidOpenSLESBackend(){
+		// Stop player playing
+		SLresult result = (*player.play)->SetPlayState(player.play, SL_PLAYSTATE_STOPPED);
+		ASSERT(res == SL_RESULT_SUCCESS);
 	}
 	
-	inline static ting::Ptr<PulseAudioBackend> New(unsigned bufferSizeFrames, aumiks::E_Format format){
-		return ting::Ptr<PulseAudioBackend>(
-				new PulseAudioBackend(bufferSizeFrames, format)
+	inline static ting::Ptr<AndroidOpenSLESBackend> New(unsigned bufferSizeFrames, aumiks::E_Format format){
+		return ting::Ptr<AndroidOpenSLESBackend>(
+				new AndroidOpenSLESBackend(bufferSizeFrames, format)
 			);
 	}
 };
