47a7d082555da2132fb4a902222d14eda375da5e
igagis <igagis@75acf142-8502-5b27-bcb6-60bf44c96ba3>
2011-12-21 15:35:09 +0000

    works somehow on android

--- android/jni/main.cpp
+++ android/jni/main.cpp
@@ -268,104 +268,109 @@ static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
  * event loop for receiving input events and doing other things.
  */
 void android_main(struct android_app* state) {
-    struct engine engine;
+	try{
+		struct engine engine;
 
-    // Make sure glue isn't stripped.
-    app_dummy();
-	
-	memset(&engine, 0, sizeof(engine));
-	
-	
-	
-	TRACE_ALWAYS(<< "STARTING!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl)
+		// Make sure glue isn't stripped.
+		app_dummy();
 
-	aumiks::Lib aumiksLibrary(100, aumiks::STEREO_16_44100);
-	
-	android_asset_file::AndroidAssetFile fi(state->activity->assetManager, "ice_break.wav");
-	engine.snd = aumiks::WavSound::LoadWAV(fi);
+		memset(&engine, 0, sizeof(engine));
 
-	ASSERT(snd)
-	
-	TRACE_ALWAYS(<< "Playing" << std::endl)
-	ting::Ref<aumiks::Channel> ch = engine.snd->Play();
-	
-	/*
-	while(ch->IsPlaying()){
-//		TRACE(<< "Loop" << std::endl)
-		ting::Thread::Sleep(50);
+
+
+		TRACE_ALWAYS(<< "STARTING!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl)
+
+		aumiks::Lib aumiksLibrary(100, aumiks::STEREO_16_44100);
+
+		android_asset_file::AndroidAssetFile fi(state->activity->assetManager, "ice_break.wav");
+		engine.snd = aumiks::WavSound::LoadWAV(fi);
+		ASSERT(engine.snd)
+
+		TRACE(<< "Playing!" << std::endl)
+		ting::Ref<aumiks::Channel> ch = engine.snd->Play();
+
+		/*
+		while(ch->IsPlaying()){
+	//		TRACE(<< "Loop" << std::endl)
+			ting::Thread::Sleep(50);
+		}
+		*/
+
+
+
+
+		state->userData = &engine;
+		state->onAppCmd = engine_handle_cmd;
+		state->onInputEvent = engine_handle_input;
+		engine.app = state;
+
+		// Prepare to monitor accelerometer
+		engine.sensorManager = ASensorManager_getInstance();
+		engine.accelerometerSensor = ASensorManager_getDefaultSensor(engine.sensorManager,
+				ASENSOR_TYPE_ACCELEROMETER);
+		engine.sensorEventQueue = ASensorManager_createEventQueue(engine.sensorManager,
+				state->looper, LOOPER_ID_USER, NULL, NULL);
+
+		if (state->savedState != NULL) {
+			// We are starting with a previous saved state; restore from it.
+			engine.state = *(struct saved_state*)state->savedState;
+		}
+
+		// loop waiting for stuff to do.
+
+		while (1) {
+			// Read all pending events.
+			int ident;
+			int events;
+			struct android_poll_source* source;
+
+			// If not animating, we will block forever waiting for events.
+			// If animating, we loop until all events are read, then continue
+			// to draw the next frame of animation.
+			while ((ident=ALooper_pollAll(engine.animating ? 0 : -1, NULL, &events,
+					(void**)&source)) >= 0) {
+
+				// Process this event.
+				if (source != NULL) {
+					source->process(state, source);
+				}
+
+				// If a sensor has data, process it now.
+				if (ident == LOOPER_ID_USER) {
+					if (engine.accelerometerSensor != NULL) {
+						ASensorEvent event;
+						while (ASensorEventQueue_getEvents(engine.sensorEventQueue,
+								&event, 1) > 0) {
+							LOGI("accelerometer: x=%f y=%f z=%f",
+									event.acceleration.x, event.acceleration.y,
+									event.acceleration.z);
+						}
+					}
+				}
+
+				// Check if we are exiting.
+				if (state->destroyRequested != 0) {
+					engine_term_display(&engine);
+					return;
+				}
+			}
+
+			if (engine.animating) {
+				// Done with events; draw next animation frame.
+				engine.state.angle += .01f;
+				if (engine.state.angle > 1) {
+					engine.state.angle = 0;
+				}
+
+				// Drawing is throttled to the screen update rate, so there
+				// is no need to do timing here.
+				engine_draw_frame(&engine);
+			}
+		}
+	}catch(std::exception& e){
+		TRACE_ALWAYS(<< "[Global exception caught]: " << e.what() << std::endl)
+	}catch(...){
+		TRACE_ALWAYS(<< "[Unknown global exception caught] " << std::endl)
 	}
-	*/
-	
-	
-	
-
-    state->userData = &engine;
-    state->onAppCmd = engine_handle_cmd;
-    state->onInputEvent = engine_handle_input;
-    engine.app = state;
-
-    // Prepare to monitor accelerometer
-    engine.sensorManager = ASensorManager_getInstance();
-    engine.accelerometerSensor = ASensorManager_getDefaultSensor(engine.sensorManager,
-            ASENSOR_TYPE_ACCELEROMETER);
-    engine.sensorEventQueue = ASensorManager_createEventQueue(engine.sensorManager,
-            state->looper, LOOPER_ID_USER, NULL, NULL);
-
-    if (state->savedState != NULL) {
-        // We are starting with a previous saved state; restore from it.
-        engine.state = *(struct saved_state*)state->savedState;
-    }
-
-    // loop waiting for stuff to do.
-
-    while (1) {
-        // Read all pending events.
-        int ident;
-        int events;
-        struct android_poll_source* source;
-
-        // If not animating, we will block forever waiting for events.
-        // If animating, we loop until all events are read, then continue
-        // to draw the next frame of animation.
-        while ((ident=ALooper_pollAll(engine.animating ? 0 : -1, NULL, &events,
-                (void**)&source)) >= 0) {
-
-            // Process this event.
-            if (source != NULL) {
-                source->process(state, source);
-            }
-
-            // If a sensor has data, process it now.
-            if (ident == LOOPER_ID_USER) {
-                if (engine.accelerometerSensor != NULL) {
-                    ASensorEvent event;
-                    while (ASensorEventQueue_getEvents(engine.sensorEventQueue,
-                            &event, 1) > 0) {
-                        LOGI("accelerometer: x=%f y=%f z=%f",
-                                event.acceleration.x, event.acceleration.y,
-                                event.acceleration.z);
-                    }
-                }
-            }
-
-            // Check if we are exiting.
-            if (state->destroyRequested != 0) {
-                engine_term_display(&engine);
-                return;
-            }
-        }
-
-        if (engine.animating) {
-            // Done with events; draw next animation frame.
-            engine.state.angle += .01f;
-            if (engine.state.angle > 1) {
-                engine.state.angle = 0;
-            }
-
-            // Drawing is throttled to the screen update rate, so there
-            // is no need to do timing here.
-            engine_draw_frame(&engine);
-        }
-    }
 }
 //END_INCLUDE(all)
