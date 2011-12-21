3496d6353f1ba2b1f28e40c93c420bb2ae88490a
igagis <igagis@75acf142-8502-5b27-bcb6-60bf44c96ba3>
2011-12-21 15:35:09 +0000

    works somehow on android

--- trunk/android/jni/Android.mk
+++ trunk/android/jni/Android.mk
@@ -20,6 +20,7 @@ LOCAL_SRC_FILES += aumiks/WavSound.cpp
 LOCAL_SRC_FILES += AndroidAssetFile.cpp
 
 LOCAL_CFLAGS := -Wno-div-by-zero #disable integer division by zero warning as it is sometimes useful when working with templates
+LOCAL_CFLAGS += -DDEBUG
 
 LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM -lOpenSLES
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
