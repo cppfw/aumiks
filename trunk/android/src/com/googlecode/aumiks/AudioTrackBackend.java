6ae12eea80b0ba79a26ee5d1e53affa36e4f1d09
igagis <igagis@75acf142-8502-5b27-bcb6-60bf44c96ba3>
2011-12-27 16:37:15 +0000

    java compiling

--- trunk/android/src/com/googlecode/aumiks/AudioTrackBackend.java
+++ trunk/android/src/com/googlecode/aumiks/AudioTrackBackend.java
@@ -2,12 +2,6 @@ package com.googlecode.aumiks;
 
 import android.app.NativeActivity;
 
-public class SharedLibLoaderNativeActivity extends NativeActivity {
-
-static {
-
-System.loadLibrary("gnustl_shared");
-System.loadLibrary("aumiks_test");
-}
-
+public class AudioTrackBackend extends NativeActivity {
+	//TODO:
 }
