d0633001f13adc8843ee4a0945a27aaf38bca113
igagis <igagis@75acf142-8502-5b27-bcb6-60bf44c96ba3>
2012-07-06 10:13:29 +0000

    some stuff

--- tests/effects/main.cpp
+++ tests/effects/main.cpp
@@ -15,7 +15,7 @@ class SineSound : public aumiks::Sound{
 		bool FillSmpBuf(ting::Buffer<ting::s32>& buf, unsigned freq, unsigned chans){
 //			TRACE_ALWAYS(<< "filling smp buf, freq = " << freq << std::endl)
 			
-			if(this->time > 100){//play sound for 100 seconds
+			if(this->time > 5){//play sound for 5 seconds
 //				TRACE_ALWAYS(<< "returned true" << std::endl)
 				return true;
 			}
@@ -56,6 +56,8 @@ public:
 
 
 
+namespace TestNonTimeDomainEffect{
+
 class VolumeEffect : public aumiks::Effect{
 	ting::Inited<volatile ting::u8, ting::u8(-1)> vol;
 public:
@@ -90,9 +92,7 @@ public:
 	}
 };
 
-
-
-int main(int argc, char *argv[]){
+void Run(){
 	TRACE_ALWAYS(<< "Opening audio playback device: Stereo 44100" << std::endl)
 	aumiks::Lib aumiksLibrary(44100, 2, 100);
 	
@@ -125,6 +125,66 @@ int main(int argc, char *argv[]){
 		vol += d * step;
 		eff->SetVolume(vol);
 	}
+}
+
+}//~namespace
+
+
+
+namespace TestResamplerEffect{
+
+class ResampleEffect : public aumiks::Effect{
+	
+	//override
+	virtual bool FillSmpBuf(ting::Buffer<ting::s32>& buf, unsigned freq, unsigned chans){
+		if(this->FillSmpBufFromNextByChain(buf, freq, chans)){
+//			TRACE_ALWAYS(<< "effect FillSmpBuf(): true from next by chain" << std::endl)
+			return true;
+		}
+		
+		//TODO:
+		return false;
+	}
+	
+public:
+	static inline ting::Ref<ResampleEffect> New(){
+		return ting::Ref<ResampleEffect>(
+				new ResampleEffect()
+			);
+	}
+	//TODO:
+};
+
+void Run(){
+	TRACE_ALWAYS(<< "Opening audio playback device: Stereo 44100" << std::endl)
+	aumiks::Lib aumiksLibrary(44100, 2, 100);
+	
+	ting::Ref<aumiks::Sound> snd = SineSound::New();
+
+	ASSERT(snd)
+	
+	ting::Ref<aumiks::Channel> ch = snd->CreateChannel();
+//	TRACE_ALWAYS(<< "ch = " << static_cast<aumiks::SampleBufferFiller*>(ch.operator->()) << std::endl)
+	
+	ting::Ref<ResampleEffect> eff = ResampleEffect::New();
+	
+	ch->AddEffect_ts(eff);
+	
+	ch->Play_ts();
+	
+	while(!ch->IsStopped_ts()){
+		ting::Thread::Sleep(333);
+	}
+}
+
+}//~namespace
+
+
+
+int main(int argc, char *argv[]){
+	
+	TestResamplerEffect::Run();
+	TestNonTimeDomainEffect::Run();
 	
 	return 0;
 }
