e40b4f4056f9cce357d379114a2e7a5dc2f593da
igagis <igagis@75acf142-8502-5b27-bcb6-60bf44c96ba3>
2014-01-15 11:26:30 +0000

    stuff

---
+++ branches/newarch/src/aumiks/rechanneler/StereoToMono.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   StereoToMono.cpp
+ * Author: igagis
+ * 
+ * Created on January 13, 2014, 5:23 PM
+ */
+
+#include "StereoToMono.hpp"
+
+StereoToMono::StereoToMono() {
+}
+
+StereoToMono::StereoToMono(const StereoToMono& orig) {
+}
+
+StereoToMono::~StereoToMono() {
+}
+
