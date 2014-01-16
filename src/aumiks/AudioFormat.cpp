b78084a31d60b29f7971c92ec0648cdd24160d59
igagis <igagis@75acf142-8502-5b27-bcb6-60bf44c96ba3>
2014-01-16 13:23:28 +0000

    stuff

---
+++ src/aumiks/AudioFormat.cpp
@@ -0,0 +1,14 @@
+/* 
+ * File:   AudioFormat.cpp
+ * Author: igagis
+ * 
+ * Created on January 16, 2014, 2:53 PM
+ */
+
+#include "AudioFormat.hpp"
+
+using namespace aumiks;
+
+AudioFormat::AudioFormat() {
+}
+
