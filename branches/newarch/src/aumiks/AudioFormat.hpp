f75963dfff985927c22f4b6e0c45bc39ce18e005
igagis <igagis@75acf142-8502-5b27-bcb6-60bf44c96ba3>
2014-01-17 17:55:03 +0000

    stuff

--- branches/newarch/src/aumiks/AudioFormat.hpp
+++ branches/newarch/src/aumiks/AudioFormat.hpp
@@ -35,17 +35,20 @@ class AudioFormat {
 public:
 	struct Channels{
 		enum Enum{
-			MONO,
-			STEREO,
-			QUADRO,
-			FIVE_DOT_ONE,
-			SEVEN_DOT_ONE
+			MONO = 1,
+			STEREO = 2,
+			QUADRO = 4,
+			FIVE_DOT_ONE = 6,
+			SEVEN_DOT_ONE = 8
 		};
 	};
 	
 	struct Frequency{
 		enum Enum{
-			//TODO:
+			HZ_11025 = 11025,
+			HZ_22050 = 22050,
+			HZ_44100 = 44100,
+			HZ_48000 = 48000
 		};
 	};
 	
