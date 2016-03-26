e6932f8e7a7d1e2267afa414ef6759cf7fb8f13f
Ivan Gagis <igagis@gmail.com>
2016-03-27 01:36:24 +0200

    stuff

---
+++ src/aumiks/Frame.hpp
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Frame.hpp
+ * Author: ivan
+ *
+ * Created on March 27, 2016, 12:52 AM
+ */
+
+#ifndef FRAME_HPP
+#define FRAME_HPP
+
+
+
+#endif /* FRAME_HPP */
+
