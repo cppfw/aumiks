9bd01369136df3e7773ff3910c72015cbeef191e
Ivan Gagis <igagis@gmail.com>
2016-04-16 16:25:29 +0300

    ios

---
+++ ios/tests/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  tests
+//
+//  Created by Ivan Gagis on 16/04/16.
+//  Copyright © 2016 Ivan Gagis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[]) {
+	return 0;
+}
